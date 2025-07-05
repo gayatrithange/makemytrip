d49afb4810e3a9c5a9dcdafe5be151a92a9e8b1c
Gayatri Sumbre <gayatri@Gayatris-MacBook-Air.local>
2025-07-05 15:41:23 +0530

    added updated pom

--- src/test/java/com/makeMyTrip/selenium_cabsTests/OutstationOneWay.java
+++ src/test/java/com/makeMyTrip/selenium_cabsTests/OutstationOneWay.java
@@ -6,6 +6,7 @@ import java.time.format.DateTimeFormatter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
@@ -105,7 +106,7 @@ public class OutstationOneWay extends BaseTest {
 		System.err.println("childTitle : " + childTitle);
 
 		if (parentTitle != childTitle)
-			AssertJUnit.assertTrue(true);
+			Assert.assertTrue(true);
 		else
 			AssertJUnit.assertTrue(false);
 
